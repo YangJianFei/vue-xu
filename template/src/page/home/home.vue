136a3ef8e843ed200182082f491757074fd87ca6
徐绍平 <455255849@qq.com>
2017-10-10 18:49:24 +0800

    更新路由模版，添加less支持

---
+++ template/src/page/home/home.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="page-home">
+      <hello></hello>
+      <p>welcome back</p>
+    </div>
+</template>
+
+<script>
+    import Hello from '../../components/HelloWorld.vue'
+    export default {
+        name: 'page-home',
+        data() {
+            return {}
+        },
+        filters: {},
+        methods: {},
+      watch: {
+        /**
+         * 一个监测路由的 request
+         */
+        '$route': function () {
+          console.log(this.$route)
+            this.$http.get('/api/url', {
+              params: {
+                id: 'params'
+              }
+            })
+            .then( res => {
+                console.log(res)
+            })
+            .catch( err => {
+              console.log(err)
+            });
+        }
+      },
+        components: {
+          Hello
+        },
+        mounted: function () {
+        },
+    }
+</script>
+
+<style lang="less">
+   @import "home";
+</style>
