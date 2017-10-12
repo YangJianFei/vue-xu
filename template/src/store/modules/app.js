91bf74118977e6fb7041c5fa837c74b3aeb3d3c5
徐绍平 <455255849@qq.com>
2017-10-12 10:14:03 +0800

    store 模版增加注释

--- template/src/store/modules/app.js
+++ template/src/store/modules/app.js
@@ -15,6 +15,17 @@ let mutations = {
 
 let actions = {};
 
+
+/**
+ *  在 *vue 中引入 ，操作变量  模块名为app.js
+ */
+
+//   this.$store.state.app.example   //获取变量
+
+//   this.$store.commit('example')  //更新变量 不带参数
+
+//   this.$store.commit('Change', total)  //更新变量 带参数
+
 export default {
   state,
   getters,
